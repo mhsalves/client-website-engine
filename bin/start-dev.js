40db76775ea4f279130e5960d26da959e8536374
Matheus Alves <matheus.alves@meliuz.com.br>
2023-03-01 01:02:47 -0400

    wip3

--- bin/start-dev.js
+++ bin/start-dev.js
@@ -1,25 +1,67 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
+const { ArgumentParser } = require('argparse');
 const concurrently = require('concurrently');
 
 const COMMAND_DEV = 'yarn dev';
 
-const workspaces = [
-  '@mhsalves/poc-client-website-remote',
-];
-
 const WORKSPACE_COMMAND_FORMAT = (workspace) => `yarn workspace ${workspace} start`;
 
-const commands = [
-  COMMAND_DEV,
-  ...workspaces.map(WORKSPACE_COMMAND_FORMAT),
-];
+function parseArgs() {
+  const parser = new ArgumentParser();
 
-console.log(commands);
+  parser.add_argument('-c', '--config', {
+    help: 'set config file of modules to be loaded in dev mode',
+    type: 'str',
+  });
 
-const { result } = concurrently(commands);
-result
-  .then(a => console.log(a))
-  .catch(e => console.error(e));
+  return parser.parse_args();
+}
 
-console.log('ddd');
+function stopExec() {
+  process.exit(1);
+}
+
+function checkParams(args) {
+  if (!args.config) stopExec();
+
+  return true;
+}
+
+function loadConfigFile(fileName) {
+  const basePath = `${__dirname}/..`;
+  const filePath = `${basePath}/${fileName}`;
+
+  try {
+    const config = fs.readFileSync(filePath, 'utf8');
+    const json = JSON.parse(config);
+
+    if (!json.engineConfig || !json.engineConfig.modules) {
+      throw new Error(`${fileName} does not in correctly format`);
+    };
+
+    return json.engineConfig;
+  } catch (error) {
+    console.error(error);
+    stopExec();
+  }
+}
+
+function run(args) {
+  checkParams(args);
+
+  const { modules } = loadConfigFile(args.config);
+
+  const commands = [
+    COMMAND_DEV,
+    ...modules.map(WORKSPACE_COMMAND_FORMAT),
+  ];
+
+  const { result } = concurrently(commands);
+  result
+    .then(a => console.log(a))
+    .catch(e => console.error(e));
+}
+
+run(parseArgs());
